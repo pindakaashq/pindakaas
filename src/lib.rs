2fc46b9d3044735269b78498eb231b63f3c8c059
Jip J. Dekker <jip@dekker.one>
2021-03-29 10:53:40 +1100

    Add definition for the Literal Trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
+use std::ops::Neg;
+use std::cmp::Eq;
+
+trait Literal: Eq + Neg<Output = Self> {}
+impl<T> Literal for T where T: Eq + Neg<Output = Self> {}
+
 #[cfg(test)]
 mod tests {
+    use crate::*;
+
+    fn is_lit<Lit: Literal>() {}
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn integer_literals() {
+        is_lit::<i8>();
+        is_lit::<i16>();
+        is_lit::<i32>();
+        is_lit::<i64>();
+        is_lit::<i128>();
     }
 }
