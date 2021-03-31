58bc8288e934d16384ea953b716f343f6295fdd1
Jip J. Dekker <jip@dekker.one>
2021-03-31 14:39:36 +1100

    Add documentation for the initial library members

--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,61 @@
 use std::cmp::Eq;
 use std::hash::Hash;
 
-/// The type used to represent boolean literals. Literals implement
-/// [`std::ops::Neg`] to allow the adding of negated literals to clauses and
-/// implement [`std::cmp::Eq`] and [`std::hash::Hash`] to allow PB constraints
-/// to be simplified.
-type Lit = i32;
+/// The type used to represent boolean literals.
+///
+/// Literals implement [`std::ops::Neg`] to allow the adding of negated literals
+/// to clauses and implement [`std::cmp::Eq`] and [`std::hash::Hash`] to allow
+/// PB constraints to be simplified.
+pub type Lit = i32;
 
+/// The `ClauseDatabase` trait is the common trait implemented by types that are
+/// used to manage the encoding of PB constraints and contain their output. This
+/// trait can be used for all encoding methods in this library.
+///
+/// This trait is a supertrait of [`ClauseSink`], which means that implementers
+/// should have a [`ClauseSink::add_clause`] method. Futhermore, implementers
+/// are required to have a [`Self::new_lit`] method.
+pub trait ClauseDatabase: ClauseSink {
+    fn new_lit(&self) -> Lit;
+}
+
+/// Types that abide by the `ClauseSink` trait can be used as the output for the
+/// constraint encodings. This trait also contains basic encodings that never
+/// create new literals.
+///
+/// To satisfy the trait, the type must implement a
+/// [`Self::add_clause`] method.
 pub trait ClauseSink {
+    /// Add a clause to the `ClauseSink`. The sink is allowed to return `false`
+    /// only when the collection of clauses has been *proven* to be
+    /// unsatisfiable. This is used as a signal to the encoder that any
+    /// subsequent encoding effort can be abandoned.
     fn add_clause(&mut self, cl: &[Lit]) -> bool;
+
+    /// Adds an encoding for an At Most One constraints to `sink` that for every
+    /// pair of literals from `lits` states that one of the literals has to be
+    /// `false`.
+    ///
+    /// # Undefined Behaviour
+    ///
+    /// - The literals in `lits` are assumed to the unique. In an AMO constraint
+    ///   non-unique literals should be preprocessed removed from the problem.
+    ///
+    /// - `lits` is expected to contain at least 2 literals. In cases where an
+    ///   AMO constraint has fewer literals, the literals can either be removed
+    ///   for the problem or the problem is already unsatisfiable
+    fn encode_naive_amo(&mut self, lits: &[Lit]) -> bool {
+        // Precondition: there is no duplicate literals
+        debug_assert!(unique_members(lits));
+        // Precondition: there are multiple literals in the AMO constraint
+        debug_assert!(lits.len() >= 2);
+        // For every pair of literals (i, j) add "¬i ∨ ¬j"
+        (0..lits.len()).into_iter().all(|i| {
+            (i+1..lits.len())
+                .into_iter()
+                .all(|j| self.add_clause(&[-lits[i], -lits[j]]))
+        })
+    }
 }
 
 impl ClauseSink for Vec<Vec<Lit>> {
@@ -26,34 +73,17 @@ impl ClauseSink for Vec<Vec<Lit>> {
     }
 }
 
-pub trait LiteralGenerator {
-    fn new_lit(&self) -> Lit;
-}
+// ---- Helper Functions ----
 
-fn is_unique<T: Hash + Eq>(collection: &[T]) -> bool {
+/// Checks if all members in a slice are unique
+fn unique_members<T: Hash + Eq>(collection: &[T]) -> bool {
+    // All members of collection are unique if a hash set constructed with the
+    // same elements has the same number of elements.
     collection
         .into_iter()
         .collect::<std::collections::HashSet<_>>()
         .len()
         == collection.len()
-}
-
-/// Adds an encoding for an At Most One constraints to `sink` that for every
-/// pair of literals from `lits` states that one of the literals has to be
-/// `false`.
-///
-/// # Undefined Behaviour
-///
-/// The literals in `lits` are assumed to the unique. In an AMO constraint
-/// non-unique literals should be preprocessed removed from the problem.
-pub fn naive_amo(lits: &[Lit], sink: &mut dyn ClauseSink) -> bool {
-    // Assume that there is no duplicate
-    debug_assert!(is_unique(lits));
-    (0..lits.len()).into_iter().all(|i| {
-        (i..lits.len())
-            .into_iter()
-            .all(|j| sink.add_clause(&[-lits[i], -lits[j]]))
-    })
 }
 
 #[cfg(test)]
