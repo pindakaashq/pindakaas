37fad252a22db9c98c9baafa576a795f51d7c909
Jip J. Dekker <jip@dekker.one>
2021-10-27 18:22:49 +1100

    Add MPL license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 //! `pindakaas` is a collection of encoders to transform pseudo-Boolean (PB)
 //! constraints into conjunctive normal form (CNF). PB constraint are in the
 //! form ∑ aᵢ·xᵢ ≷ k, where the aᵢ's and k are constant, xᵢ's are boolean
