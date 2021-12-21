babc21b6aa373b521fa90f78ec2ae151c3f92e26
Jip J. Dekker <jip@dekker.one>
2021-12-21 11:01:12 +1100

    Add Python type to easily define a next literal function

--- crates/python/src/lib.rs
+++ crates/python/src/lib.rs
@@ -1,14 +1,50 @@
-use pyo3::prelude::*;
+use pyo3::{exceptions::PyTypeError, prelude::*};
 
-#[pyfunction]
-fn double(x: usize) -> usize {
-	x * 2
+#[derive(Debug, Clone)]
+enum NewLitFn<'a> {
+	SeqFrom(i32),
+	Call(&'a PyAny),
+}
+
+impl<'source> FromPyObject<'source> for NewLitFn<'source> {
+	fn extract(obj: &'source PyAny) -> PyResult<Self> {
+		if obj.is_callable() {
+			return Ok(NewLitFn::Call(obj));
+		}
+		if let Ok(x) = obj.extract::<i32>() {
+			return Ok(NewLitFn::SeqFrom(x));
+		}
+		Err(PyTypeError::new_err(
+			"Expected function or int to give the next new literal",
+		))
+	}
+}
+
+impl<'a> NewLitFn<'a> {
+	fn next_lit(&mut self) -> i32 {
+		match self {
+			NewLitFn::SeqFrom(next) => {
+				let y = next.clone();
+				*next += 1;
+				y
+			}
+			NewLitFn::Call(fun) => match fun.call0() {
+				Ok(obj) => match obj.extract() {
+					Ok(next) => next,
+					Err(err) => panic!("New literal function did not return an int: {}", err),
+				},
+				Err(err) => panic!(
+					"New literal function could not be called correctly: {}",
+					err
+				),
+			},
+		}
+	}
 }
 
 #[pymodule]
 #[pyo3(name = "pindakaas")]
-fn module(_py: Python, m: &PyModule) -> PyResult<()> {
-	m.add_function(wrap_pyfunction!(double, m)?)?;
+fn module(_py: Python, _m: &PyModule) -> PyResult<()> {
 	Ok(())
 }
 
