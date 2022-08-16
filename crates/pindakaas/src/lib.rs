715eee1e72f0021f2021f1629ff47a9aebc5b2a3
Jip J. Dekker <jip@dekker.one>
2022-08-16 13:27:48 +1000

    Remove MPL-2 file headers

--- crates/pindakaas/src/lib.rs
+++ crates/pindakaas/src/lib.rs
@@ -1,7 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 //! `pindakaas` is a collection of encoders to transform integer and
 //! pseudo-Boolean (PB) constraints into conjunctive normal form (CNF). It
 //! currently considers mostly linear constraints, which are in the form ∑
