dccbbd12faf8582c733754bec16a2d4b35419efc
Jip J. Dekker <jip@dekker.one>
2022-08-16 13:27:48 +1000

    Remove MPL-2 file headers

--- crates/pindacaas/src/lib.rs
+++ crates/pindacaas/src/lib.rs
@@ -1,7 +1,3 @@
-// This Source Code Form is subject to the terms of the Mozilla Public
-// License, v. 2.0. If a copy of the MPL was not distributed with this
-// file, You can obtain one at http://mozilla.org/MPL/2.0/.
-
 use libc::size_t;
 use pindakaas::{ClauseDatabase, ClauseSink, Result, Unsatisfiable};
 use std::{ffi::c_void, slice};
