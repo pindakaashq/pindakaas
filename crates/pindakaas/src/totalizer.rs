61ae0f2b88dff50c73526dcf3d9f87f4b74f6464
hbierlee <bierlee.henk@gmail.com>
2022-09-14 14:49:07 +1000

    Refactor totalizer, removing empty node for odd nbr of leaves

--- crates/pindakaas/src/totalizer.rs
+++ crates/pindakaas/src/totalizer.rs
@@ -14,15 +14,39 @@ pub fn encode_bool_lin_le_totalizer<
 ) -> Result {
 	debug_assert!(cmp == Comparator::LessEq);
 	// Every layer of the totalizer (binary) tree has nodes containing literals associated with (unique) values (which equal the sum so far)
+
 	let root = build_totalizer(
 		partition
 			.iter()
 			.map(|part| {
-				let terms: Vec<(PC, Lit)> = match part {
-					Part::Amo(terms) => terms
-						.iter()
-						.map(|(lit, coef)| (*coef, lit.clone()))
-						.collect(),
+				match part {
+					Part::Amo(terms) => {
+						let terms: Vec<(PC, Lit)> = terms
+							.iter()
+							.map(|(lit, coef)| (*coef, lit.clone()))
+							.collect();
+
+						// for a set of terms with the same coefficients, replace by a single term with fresh variable o (implied by each literal)
+						let mut h: HashMap<PC, Vec<Lit>> = HashMap::with_capacity(terms.len());
+						for (coef, lit) in terms {
+							h.entry(coef).or_insert_with(Vec::new).push(lit);
+						}
+
+						h.into_iter()
+							.map(|(coef, lits)| {
+								if lits.len() == 1 {
+									(coef, lits[0].clone())
+								} else {
+									let o = db.new_var();
+									for lit in lits {
+										db.add_clause(&[lit.negate(), o.clone()]).unwrap();
+									}
+									(coef, o)
+								}
+							})
+							.collect()
+					}
+					// Leaves built from Ic/Dom groups are guaranteed to have unique values
 					Part::Ic(terms) => {
 						let mut acc = PC::zero(); // running sum
 						terms
@@ -34,7 +58,6 @@ pub fn encode_bool_lin_le_totalizer<
 							.collect()
 					}
 					Part::Dom(terms, l, u) => {
-						// totalizer root has unique values, so just return
 						return build_totalizer(
 							terms
 								.iter()
@@ -45,27 +68,7 @@ pub fn encode_bool_lin_le_totalizer<
 							*u,
 						);
 					}
-				};
-
-				// for a set of terms with the same coefficients, replace by a single term with fresh variable o (implied by each literal)
-				let mut h: HashMap<PC, Vec<Lit>> = HashMap::with_capacity(terms.len());
-				for (coef, lit) in terms {
-					h.entry(coef).or_insert_with(Vec::new).push(lit);
 				}
-
-				h.into_iter()
-					.map(|(coef, lits)| {
-						if lits.len() == 1 {
-							(coef, lits[0].clone())
-						} else {
-							let o = db.new_var();
-							for lit in lits {
-								db.add_clause(&[lit.negate(), o.clone()]).unwrap();
-							}
-							(coef, o)
-						}
-					})
-					.collect()
 			})
 			.collect(),
 		db,
@@ -87,52 +90,53 @@ fn build_totalizer<
 	DB: ClauseDatabase<Lit = Lit> + ?Sized,
 	PC: PositiveCoefficient,
 >(
-	mut layer: Vec<HashMap<PC, Lit>>,
+	layer: Vec<HashMap<PC, Lit>>,
 	db: &mut DB,
 	_l: PC,
 	u: PC,
 ) -> HashMap<PC, Lit> {
-	loop {
-		// Fix case of odd number of leaf nodes; by adding an empty right-hand node, we will get a correct parent node
-		if layer.len() % 2 == 1 {
-			layer.push(HashMap::new());
-		}
-
-		// merge two adjacent nodes of the current layer into one parent node
-		layer = layer
-			.chunks(2)
-			.map(|children| {
-				// TODO if !right.is_empty(), return left (or chain left to this iff odd or something  ..)
-				let mut parent = HashMap::new();
-				let (left, right) = (&children[0], &children[1]);
-
-				// any child lit implies the parent lit with the same value
-				for c in left.iter().chain(right.iter()) {
-					let w = std::cmp::min(*c.0, u + PC::one()); // not capped in literature, but should be slightly better
-					let p = parent.entry(w).or_insert_with(|| db.new_var());
-					// TODO we do not need to create nodes where w<l (but current vars need to be passed on)
-					db.add_clause(&[c.1.negate(), p.clone()]).unwrap();
-				}
-
-				// two lits together imply the parent lit with the sum of their values
-				// TODO can be optimised if by sorting both children by value
-				for a in left {
-					for b in right {
-						let w = std::cmp::min(*a.0 + *b.0, u);
-						let p = parent.entry(w).or_insert_with(|| db.new_var());
-						// TODO figure out what to do if w<l here as well.
-						db.add_clause(&[a.1.negate(), b.1.negate(), p.clone()])
-							.unwrap();
+	if layer.len() == 1 {
+		layer.into_iter().next().unwrap()
+	} else {
+		build_totalizer(
+			layer
+				.chunks(2)
+				.map(|children| {
+					if children.len() == 1 {
+						return children[0].clone();
 					}
-				}
 
-				parent
-			})
-			.collect();
+					let (left, right) = (&children[0], &children[1]);
 
-		if layer.len() == 1 {
-			return layer.pop().unwrap();
-		}
+					// any child lit implies the parent lit with the same value
+					let mut parent = HashMap::<PC, Lit>::new();
+					for c in left.iter().chain(right.iter()) {
+						let p = parent.entry(*c.0).or_insert_with(|| db.new_var());
+						// TODO we do not need to create nodes where w<l (but current vars need to be passed on)
+						eprintln!("{:?} -> {:?}", c.0, c.0);
+						db.add_clause(&[c.1.negate(), p.clone()]).unwrap();
+					}
+
+					// two lits together imply the parent lit with the sum of their values
+					// TODO can be optimised if by sorting both children by value
+					for a in left.iter() {
+						for b in right.iter() {
+							let w = std::cmp::min(*a.0 + *b.0, u);
+							eprintln!("{:?} and {:?} -> {:?}", a.0, b.0, w);
+							let p = parent.entry(w.clone()).or_insert_with(|| db.new_var());
+							// TODO figure out what to do if w<l here as well.
+							db.add_clause(&[a.1.negate(), b.1.negate(), p.clone()])
+								.unwrap();
+						}
+					}
+
+					parent
+				})
+				.collect(),
+			db,
+			_l,
+			u,
+		)
 	}
 }
 
