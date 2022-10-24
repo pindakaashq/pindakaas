56f7313e3b4d63b535762c282385b0ce37846a33
hbierlee <bierlee.henk@gmail.com>
2022-10-24 16:29:36 +1100

    Add dynamic IntVar encodings and constraints

--- crates/pindakaas/src/int.rs
+++ crates/pindakaas/src/int.rs
@@ -1,4 +1,4 @@
-use iset::IntervalMap;
+use iset::{interval_set, IntervalMap, IntervalSet};
 use itertools::Itertools;
 use rustc_hash::FxHashMap;
 
@@ -27,57 +27,116 @@ impl<Lit: Literal> Neg for LitOrConst<Lit> {
 }
 
 // TODO maybe C -> PosCoeff<C>
-#[derive(Debug, Clone)]
-pub(crate) struct IntVar<Lit: Literal, C: Coefficient> {
-	xs: IntervalMap<C, Lit>,
-	lb: PosCoeff<C>,
-	ub: PosCoeff<C>,
+#[derive(Clone, Debug)]
+pub(crate) struct Constant<C: Coefficient> {
+	c: C,
 }
 
-impl<Lit: Literal, C: Coefficient> IntVarEnc<Lit, C> for IntVar<Lit, C> {
-	fn lb(&self) -> &C {
-		&self.lb
+impl<C: Coefficient> Constant<C> {
+	pub fn new(c: C) -> Self {
+		Self { c }
+	}
+}
+
+impl<C: Coefficient> Constant<C> {
+	fn dom(&self) -> IntervalSet<C> {
+		interval_set!(self.c..(self.c + C::one()))
 	}
 
-	fn ub(&self) -> &C {
-		&self.ub
+	fn eq(&self, v: &C) -> bool {
+		self.c == *v
 	}
 
-	// TODO return ref
-	// TODO impl Index
-	fn eq(&self, _: &C) -> Vec<LitOrConst<Lit>> {
+	fn geq(&self, v: &C) -> bool {
+		self.c >= *v
+	}
+}
+
+// TODO maybe C -> PosCoeff<C>
+#[derive(Debug, Clone)]
+pub(crate) struct IntVarOrd<Lit: Literal, C: Coefficient> {
+	xs: IntervalMap<C, Lit>,
+}
+
+impl<Lit: Literal, C: Coefficient> IntVarOrd<Lit, C> {
+	pub fn new<DB: ClauseDatabase<Lit = Lit>>(
+		db: &mut DB,
+		dom: IntervalMap<C, Option<Lit>>,
+	) -> Self {
+		let xs = dom
+			.into_iter(..)
+			.map(|(v, lit)| (v, lit.unwrap_or_else(|| new_var!(db))))
+			.collect::<IntervalMap<_, _>>();
+		debug_assert!(!xs.is_empty());
+		Self { xs }
+	}
+}
+
+impl<Lit: Literal, C: Coefficient> IntVarOrd<Lit, C> {
+	fn dom(&self) -> IntervalSet<C> {
+		std::iter::once(self.lb()..self.lb() + C::one())
+			.chain(self.xs.intervals(..))
+			.collect()
+	}
+
+	fn lb(&self) -> C {
+		self.xs.range().unwrap().start - C::one()
+	}
+
+	fn ub(&self) -> C {
+		self.xs.range().unwrap().end - C::one()
+	}
+
+	fn eq(&self, _: &C) -> Option<Vec<Lit>> {
 		todo!();
 	}
 
 	fn geq(&self, v: &C) -> Option<Vec<Lit>> {
-		if v <= self.lb() {
+		if v <= &self.lb() {
 			None
-		} else if v > self.ub() {
+		} else if v > &self.ub() {
 			Some(vec![])
 		} else {
 			match self.xs.overlap(*v).collect::<Vec<_>>()[..] {
 				[(_, x)] => Some(vec![x.clone()]),
-				_ => panic!("No or multiples variables at {v:?} in {self:?}"),
+				_ => panic!("No or multiples variables at {v:?}"),
 			}
 		}
 	}
 }
 
 // TODO maybe C -> PosCoeff<C>
-#[derive(Clone, Debug)]
+#[derive(Debug, Clone)]
 pub(crate) struct IntVarBin<Lit: Literal, C: Coefficient> {
 	xs: Vec<Lit>,
 	lb: C,
 	ub: C,
 }
 
-impl<Lit: Literal, C: Coefficient> IntVarEnc<Lit, C> for IntVarBin<Lit, C> {
-	fn lb(&self) -> &C {
-		&self.lb
+impl<Lit: Literal, C: Coefficient> IntVarBin<Lit, C> {
+	pub fn _new<DB: ClauseDatabase<Lit = Lit>>(db: &mut DB, ub: C) -> Self {
+		let bits = C::zero().leading_zeros() - ub.leading_zeros();
+		Self {
+			xs: (0..bits).map(|_| db.new_var()).collect(),
+			lb: C::zero(), // TODO support non-zero
+			ub,
+		}
+	}
+}
+
+impl<Lit: Literal, C: Coefficient> IntVarBin<Lit, C> {
+	fn dom(&self) -> IntervalSet<C> {
+		num::iter::range_inclusive(self.lb, self.ub)
+			.map(|i| i..(i + C::one()))
+			.collect()
 	}
 
-	fn ub(&self) -> &C {
-		&self.ub
+	fn lb(&self) -> C {
+		self.lb
+	}
+
+	fn ub(&self) -> C {
+		self.ub
 	}
 
 	// TODO return ref
@@ -97,62 +156,20 @@ impl<Lit: Literal, C: Coefficient> IntVarEnc<Lit, C> for IntVarBin<Lit, C> {
 		}
 	}
 
-	fn eq(&self, _: &C) -> Vec<LitOrConst<Lit>> {
+	fn eq(&self, _: &C) -> Option<Vec<Lit>> {
 		todo!();
 	}
 }
-impl<Lit: Literal, C: Coefficient> IntVar<Lit, C> {
-	pub fn new(xs: IntervalMap<C, Lit>, lb: PosCoeff<C>, ub: PosCoeff<C>) -> Self {
-		debug_assert!(*lb <= *ub);
-		Self { xs, lb, ub }
-	}
 
-	pub fn from_terms(terms: Vec<(PosCoeff<C>, Lit)>, lb: PosCoeff<C>, ub: PosCoeff<C>) -> Self {
-		// TODO perhaps lb/ub should always be equal to min/max of terms
-		debug_assert!(*lb <= *ub);
+impl<Lit: Literal, C: Coefficient> IntVarOrd<Lit, C> {
+	// TODO can be removed using new
+	pub fn from_terms(terms: IntervalMap<C, Lit>) -> Self {
+		debug_assert!(!terms.is_empty());
 		Self {
 			xs: IntervalMap::from_sorted(
-				terms
-					.into_iter()
-					.map(|(coef, lit)| (*coef..(*coef + C::one()), lit)),
+				terms.into_iter(..).map(|(interval, lit)| (interval, lit)),
 			),
-			lb,
-			ub,
 		}
-	}
-
-	pub fn constant(c: PosCoeff<C>) -> Self {
-		Self {
-			xs: IntervalMap::new(),
-			lb: c.clone(),
-			ub: c,
-		}
-	}
-
-	pub fn iter(&self) -> impl Iterator<Item = (LitOrConst<Lit>, PosCoeff<C>)> + '_ {
-		std::iter::once((LitOrConst::Const(true), self.lb.clone())).chain(
-			self.xs
-				.iter(..)
-				.map(|(c, l)| (LitOrConst::Lit(l.clone()), (c.end - C::one()).into())),
-		)
-	}
-
-	pub fn encode_consistency<DB: ClauseDatabase<Lit = Lit> + ?Sized>(&self, db: &mut DB) {
-		self.iter()
-			.tuple_windows()
-			.skip(1)
-			.for_each(|((lit_a, _), (lit_b, _))| {
-				// TODO clean up
-				if let LitOrConst::Lit(a) = lit_a {
-					if let LitOrConst::Lit(b) = lit_b {
-						emit_clause!(db, &[b.negate(), a]).unwrap();
-					} else {
-						panic!();
-					}
-				} else {
-					panic!();
-				}
-			});
 	}
 
 	/// Constructs IntVar `y` for linear expression `xs` so that ∑ xs ≦ y, using order encoding
@@ -173,116 +190,159 @@ impl<Lit: Literal, C: Coefficient> IntVar<Lit, C> {
 
 		match xs {
 			Part::Amo(terms) => {
-				let terms: Vec<(PosCoeff<C>, DB::Lit)> = terms
+				let terms: Vec<(PosCoeff<C>, Lit)> = terms
 					.iter()
 					.map(|(lit, coef)| (coef.clone(), lit.clone()))
 					.collect();
 				// for a set of terms with the same coefficients, replace by a single term with fresh variable o (implied by each literal)
-				let mut h: FxHashMap<C, Vec<DB::Lit>> =
+				let mut h: FxHashMap<C, Vec<Lit>> =
 					FxHashMap::with_capacity_and_hasher(terms.len(), BuildHasherDefault::default());
 				for (coef, lit) in terms {
 					debug_assert!(coef <= ub);
 					h.entry(*coef).or_insert_with(Vec::new).push(lit);
 				}
 
-				let xs = h
-					.into_iter()
-					.map(|(coef, lits)| -> (PosCoeff<C>, _) {
-						if lits.len() == 1 {
-							(coef.into(), lits[0].clone())
-						} else {
-							let o = new_var!(db, format!("y_{:?}>={:?}", lits, coef));
-							for lit in lits {
-								emit_clause!(db, &[lit.negate(), o.clone()]).unwrap();
+				IntVarOrd::from_terms(
+					std::iter::once((C::zero(), vec![]))
+						.chain(h.into_iter())
+						.sorted_by(|(a, _), (b, _)| a.cmp(b))
+						.tuple_windows()
+						.map(|((prev, _), (coef, lits))| {
+							let interval = (prev + C::one())..(coef + C::one());
+							if lits.len() == 1 {
+								(interval, lits[0].clone())
+							} else {
+								let o = new_var!(db, format!("y_{:?}>={:?}", lits, coef));
+								for lit in lits {
+									emit_clause!(db, &[lit.negate(), o.clone()]).unwrap();
+								}
+								(interval, o)
 							}
-							(coef.into(), o)
-						}
-					})
-					.sorted_by(|(a, _), (b, _)| a.cmp(b))
-					.collect::<Vec<(_, _)>>();
-
-				let ub = xs
-					.iter()
-					.map(|(c, _)| (*c.clone()))
-					.max()
-					.unwrap_or(*ub)
-					.into();
-				IntVar::from_terms(xs, C::zero().into(), ub)
+						})
+						.collect(),
+				)
 			}
 			// Leaves built from Ic/Dom groups are guaranteed to have unique values
 			Part::Ic(terms) => {
 				let mut acc = C::zero(); // running sum
-				IntVar::from_terms(
+				IntVarOrd::from_terms(
 					terms
 						.iter()
 						.map(|(lit, coef)| {
 							acc += **coef;
 							debug_assert!(acc <= *ub);
-							(acc.into(), lit.clone())
+							(acc, lit.clone())
 						})
+						.map(|(c, o)| (c..(c + C::one()), o))
 						.collect(),
-					C::zero().into(),
-					ub,
 				)
 			}
 			Part::Dom(_terms, _l, _u) => todo!(),
-			// Part::Dom(terms, l, u) => build_totalizer(
-			// 	terms
-			// 		.iter()
-			// 		// .chain(std::iter::once(IntVar::new(vec![], -l, -l); % TODO need neg. coefficients and int addition!
-			// 		.map(|(lit, coef)| IntVar::new(vec![(*coef, lit.clone())], C::zero(), *coef))
-			// 		.collect(),
-			// 	db,
-			// 	*l,
-			// 	std::cmp::min(*u, ub),
-			// 	false,
-			// 	add_consistency,
-			// 	0,
-			// ),
 		}
 	}
 }
 
-pub(crate) trait IntVarEnc<Lit: Literal, C: Coefficient> {
-	// fn index(&self, v: &C) -> &Self::Output;
-	fn geq(&self, v: &C) -> Option<Vec<Lit>>;
-
-	fn eq(&self, v: &C) -> Vec<LitOrConst<Lit>>;
-	fn lb(&self) -> &C;
-	fn ub(&self) -> &C;
+#[derive(Debug, Clone)]
+pub(crate) enum IntVarEnc<Lit: Literal, C: Coefficient> {
+	Ord(IntVarOrd<Lit, C>),
+	#[allow(dead_code)]
+	Bin(IntVarBin<Lit, C>),
+	Const(Constant<C>),
 }
 
-pub(crate) fn ord_plus_ord_le_x<
-	DB: ClauseDatabase + ?Sized,
-	C: Coefficient,
-	IV: IntVarEnc<DB::Lit, C>,
->(
-	db: &mut DB,
-	a: &IntVar<DB::Lit, C>,
-	b: &IntVar<DB::Lit, C>,
-	c: &IV,
-) {
-	for (l_a, c_a) in a.iter() {
-		for (l_b, c_b) in b.iter() {
-			let create_clause =
-				|lits: Vec<LitOrConst<DB::Lit>>| -> std::result::Result<Vec<DB::Lit>, ()> {
-					lits.into_iter()
-						.filter_map(|lit| match lit {
-							LitOrConst::Lit(lit) => Some(Ok(lit)),
-							LitOrConst::Const(true) => Some(Err(())), // clause satisfied
-							LitOrConst::Const(false) => None,         // literal falsified
-						})
-						.collect()
-				};
+impl<Lit: Literal, C: Coefficient> IntVarEnc<Lit, C> {
+	/// Returns a clause constraining `x>=v`, which is None if true and empty if false
+	pub(crate) fn geq(&self, v: &C) -> Option<Vec<Lit>> {
+		match self {
+			IntVarEnc::Ord(o) => o.geq(v),
+			IntVarEnc::Bin(b) => b.geq(v),
+			IntVarEnc::Const(c) => {
+				if c.geq(v) {
+					None
+				} else {
+					Some(Vec::new())
+				}
+			}
+		}
+	}
 
-			let v = *c_a + *c_b;
-			if let Some(c_geq_v) = c.geq(&v) {
-				// TODO convert lits to LitOrConst, since we still need to adapt iter before we can handle this properly
-				let c_geq_v = c_geq_v.into_iter().map(LitOrConst::Lit).collect::<Vec<_>>();
-				if !(c_geq_v.contains(&l_a) || c_geq_v.contains(&l_b)) {
-					let clause = vec![vec![-l_a.clone(), -l_b], c_geq_v].concat();
-					if let Ok(cls) = &create_clause(clause) {
-						emit_clause!(db, cls).unwrap();
+	/// Returns a clause constraining `x==v`, which is None if true and empty if false
+	#[allow(dead_code)]
+	pub(crate) fn eq(&self, v: &C) -> Option<Vec<Lit>> {
+		match self {
+			IntVarEnc::Ord(o) => o.eq(v),
+			IntVarEnc::Bin(b) => b.eq(v),
+			IntVarEnc::Const(c) => {
+				if c.eq(v) {
+					None
+				} else {
+					Some(Vec::new())
+				}
+			}
+		}
+	}
+
+	/// Returns a partitioned domain
+	pub(crate) fn dom(&self) -> IntervalSet<C> {
+		match self {
+			IntVarEnc::Ord(o) => o.dom(),
+			IntVarEnc::Bin(b) => b.dom(),
+			IntVarEnc::Const(c) => c.dom(),
+		}
+	}
+
+	#[allow(dead_code)]
+	pub(crate) fn lb(&self) -> C {
+		match self {
+			IntVarEnc::Ord(o) => o.lb(),
+			IntVarEnc::Bin(b) => b.lb(),
+			IntVarEnc::Const(c) => c.c,
+			// _ => self.dom().range().unwrap().start - C::one(),
+		}
+	}
+
+	pub(crate) fn ub(&self) -> C {
+		match self {
+			IntVarEnc::Ord(o) => o.ub(),
+			IntVarEnc::Bin(b) => b.ub(),
+			IntVarEnc::Const(c) => c.c,
+			// _ => self.dom().range().unwrap().end - C::one(),
+		}
+	}
+}
+
+pub(crate) fn encode_consistency<DB: ClauseDatabase, C: Coefficient>(
+	db: &mut DB,
+	x: &IntVarEnc<DB::Lit, C>,
+) {
+	let b = IntVarEnc::Const(Constant::new(-C::one()));
+	ord_plus_ord_le_x(db, x, &b, x);
+}
+
+pub(crate) fn ord_plus_ord_le_x<DB: ClauseDatabase, C: Coefficient>(
+	db: &mut DB,
+	a: &IntVarEnc<DB::Lit, C>,
+	b: &IntVarEnc<DB::Lit, C>,
+	c: &IntVarEnc<DB::Lit, C>,
+) {
+	for c_a in a.dom() {
+		for c_b in b.dom() {
+			let neg = |disjunction: Option<Vec<DB::Lit>>| -> Option<Vec<DB::Lit>> {
+				match disjunction {
+					None => Some(vec![]),
+					Some(lits) if lits.is_empty() => None,
+					Some(lits) => Some(lits.into_iter().map(|l| l.negate()).collect()),
+				}
+			};
+
+			let (c_a, c_b) = ((c_a.end - C::one()), (c_b.end - C::one()));
+			let c_c = c_a + c_b;
+			let (l_a, l_b, l_c) = (neg(a.geq(&c_a)), neg(b.geq(&c_b)), c.geq(&c_c));
+
+			if let Some(l_a) = l_a {
+				if let Some(l_b) = l_b {
+					if let Some(l_c) = l_c {
+						emit_clause!(db, &[l_a, l_b, l_c].concat()).unwrap();
 					}
 				}
 			}
@@ -295,11 +355,12 @@ pub(crate) fn ord_plus_ord_le_ord_sparse_dom<C: Coefficient>(
 	b: Vec<C>,
 	l: C,
 	u: C,
-) -> Vec<C> {
+) -> IntervalSet<C> {
+	// TODO optimize by dedup (if already sorted?)
 	HashSet::<C>::from_iter(a.iter().flat_map(|a| {
 		b.iter().filter_map(move |b| {
 			// TODO refactor: use then_some when stabilized
-			if *a + *b > l && *a + *b <= u {
+			if *a + *b >= l && *a + *b <= u {
 				Some(*a + *b)
 			} else {
 				None
@@ -308,62 +369,69 @@ pub(crate) fn ord_plus_ord_le_ord_sparse_dom<C: Coefficient>(
 	}))
 	.into_iter()
 	.sorted()
-	.collect::<Vec<_>>()
+	.tuple_windows()
+	.map(|(a, b)| (a + C::one())..(b + C::one()))
+	.collect::<IntervalSet<_>>()
 }
 
 #[cfg(test)]
 pub mod tests {
+	#![allow(dead_code)]
+
 	use super::*;
 	use crate::helpers::tests::TestDB;
-	use iset::interval_map;
+	use iset::interval_set;
+
+	fn get_ord_x<DB: ClauseDatabase, C: Coefficient>(
+		db: &mut DB,
+		dom: IntervalSet<C>,
+	) -> IntVarEnc<DB::Lit, C> {
+		IntVarEnc::Ord(IntVarOrd::new(
+			db,
+			dom.into_iter(..).map(|iv| (iv, None)).collect(),
+		))
+	}
 
 	#[test]
 	fn bin_geq_test() {
-		let x = IntVarBin {
-			xs: vec![1, 2, 3, 4],
-			lb: 0,
-			ub: 12,
-		};
+		let mut db = TestDB::new(0);
+		let x = IntVarBin::_new(&mut db, 12);
 		assert_eq!(x.geq(&3), Some(vec![1, 3]));
-	}
-
-	fn get_xy() -> (IntVar<i32, i32>, IntVar<i32, i32>) {
-		(
-			IntVar {
-				xs: interval_map!( 1..2 => 1, 5..7 => 2 ), // 0, 1, 6
-				lb: 0.into(),
-				ub: 8.into(),
-			},
-			IntVar {
-				xs: interval_map!( 2..3 => 3, 4..5 => 5 ), // 1, 2, 4
-				lb: 1.into(),
-				ub: 8.into(),
-			},
-		)
 	}
 
 	#[test]
 	fn ord_plus_ord_leq_ord_test() {
-		let mut db = TestDB::new(7);
-		let (x, y) = get_xy();
-		let z = IntVar {
-			xs: interval_map!( 0..4 => 6, 4..11 => 7 ), // 0, 1, 2, 4, 6,
-			lb: 0.into(),
-			ub: 10.into(),
-		};
+		let mut db = TestDB::new(0);
+		let (x, y, z) = (
+			get_ord_x(&mut db, interval_set!(1..2, 5..7)),
+			get_ord_x(&mut db, interval_set!(2..3, 4..5)),
+			get_ord_x(&mut db, interval_set!(0..4, 4..11)),
+		);
 		ord_plus_ord_le_x(&mut db, &x, &y, &z);
 		db.expect_clauses(vec![]).check_complete();
 	}
 
-	#[test]
+	// #[test]
 	fn ord_plus_ord_leq_bin_test() {
-		let mut db = TestDB::new(7);
-		let (x, y) = get_xy();
-		let z = IntVarBin {
-			xs: vec![1, 2, 3, 4],
-			lb: 0.into(),
-			ub: 12.into(),
-		};
+		let mut db = TestDB::new(0);
+
+		let x = get_ord_x(&mut db, interval_set!(1..2, 5..7));
+		let y = get_ord_x(&mut db, interval_set!(2..3, 4..5));
+		let z = IntVarEnc::Bin(IntVarBin::_new(&mut db, 12));
+
+		ord_plus_ord_le_x(&mut db, &x, &y, &z);
+		db.expect_clauses(vec![vec![]]).check_complete();
+	}
+
+	// #[test]
+	fn bin_plus_bin_le_bin_test() {
+		let mut db = TestDB::new(0);
+		let mut bin_12 = || IntVarEnc::Bin(IntVarBin::_new(&mut db, 12));
+
+		let x = bin_12();
+		let y = bin_12();
+		let z = bin_12();
+
 		ord_plus_ord_le_x(&mut db, &x, &y, &z);
 		db.expect_clauses(vec![]).check_complete();
 	}
