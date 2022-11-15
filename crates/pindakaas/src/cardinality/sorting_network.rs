467e4988caa270462e3ac8f3696ac247a8227250
hbierlee <bierlee.henk@gmail.com>
2022-11-15 20:53:59 +1100

    Add and fix more Sorted tests

--- crates/pindakaas/src/cardinality/sorting_network.rs
+++ crates/pindakaas/src/cardinality/sorting_network.rs
@@ -1,52 +1,34 @@
 use crate::{
-	linear::LimitComp,
+	sorted::{Sorted, SortedEncoder},
 	trace::{emit_clause, new_var},
-	Cardinality, ClauseDatabase, Coefficient, Encoder, Literal, Result,
+	Cardinality, ClauseDatabase, Coefficient, Encoder, Result,
 };
-
-use itertools::{Itertools, Position};
 
 /// Encoder for the linear constraints that ∑ litsᵢ ≷ k using a sorting network
 #[derive(Default)]
 pub struct SortingNetworkEncoder {
-	add_consistency: bool,
+	sorted_encoder: SortedEncoder,
 }
 
 impl SortingNetworkEncoder {
-	pub fn add_consistency(&mut self, b: bool) -> &mut Self {
-		self.add_consistency = b;
+	pub fn set_sorted_encoder(&mut self, sorted_encoder: SortedEncoder) -> &mut Self {
+		self.sorted_encoder = sorted_encoder;
 		self
 	}
 }
-
-use crate::sorted::{Sorted, SortedEncoder};
 
 impl<DB: ClauseDatabase, C: Coefficient> Encoder<DB, Cardinality<DB::Lit, C>>
 	for SortingNetworkEncoder
 {
 	fn encode(&mut self, db: &mut DB, card: &Cardinality<DB::Lit, C>) -> Result {
-		let ys = card
-			.lits
-			.iter()
-			.take((*card.k + C::one()).to_usize().unwrap())
-			.map(|_x| new_var!(db, format!("y_{_x:?}"))) // TODO: Use label of _x
+		let n = (0..card.lits.len()).fold(C::one(), |a, _| a + C::one());
+		let ys = num::range_inclusive(C::one(), std::cmp::min(n, *card.k))
+			.map(|_| new_var!(db, format!("y_{:?}", &card.lits[0]))) // TODO: Use label
 			.collect::<Vec<_>>();
-		let sorted = Sorted::new(card.lits.as_slice(), LimitComp::Equal, &ys);
-		SortedEncoder::default()
-			.add_consistency(self.add_consistency)
-			.encode(db, &sorted)?;
+		let sorted = Sorted::new(card.lits.as_slice(), card.cmp.clone(), &ys);
+		self.sorted_encoder.encode(db, &sorted)?;
 
-		if card.cmp == LimitComp::Equal {
-			for y in ys.into_iter().with_position() {
-				match y {
-					Position::First(y) | Position::Middle(y) => emit_clause!(db, &[y])?,
-					Position::Only(y) | Position::Last(y) => emit_clause!(db, &[y.negate()])?,
-				}
-			}
-		} else {
-			emit_clause!(db, &[ys[ys.len() - 1].negate()])?;
-		}
-		Ok(())
+		ys.into_iter().try_for_each(|y| emit_clause!(db, &[y]))
 	}
 }
 
@@ -54,9 +36,119 @@ impl<DB: ClauseDatabase, C: Coefficient> Encoder<DB, Cardinality<DB::Lit, C>>
 mod tests {
 	use super::*;
 	use crate::{
-		cardinality::tests::card_test_suite, cardinality_one::tests::card1_test_suite,
-		helpers::tests::assert_sol, linear::LimitComp, Cardinality, CardinalityOne, Encoder,
+		helpers::tests::assert_sol,
+		linear::LimitComp,
+		sorted::{SortedEncoder, SortedStrategy},
+		Cardinality, Encoder,
 	};
-	card1_test_suite!(SortingNetworkEncoder::default());
-	card_test_suite!(SortingNetworkEncoder::default());
+
+	macro_rules! test_card {
+		($encoder:expr,$n:expr,$cmp:expr,$k:expr) => {
+			assert_sol!(
+				$encoder,
+				$n,
+				&Cardinality {
+					lits: (1..=$n).collect(),
+					cmp: $cmp,
+					k: $k.into()
+				}
+			);
+		};
+	}
+
+	macro_rules! sorted_card_test_suite {
+		($encoder:expr,$cmp:expr) => {
+			use super::*;
+
+			#[test]
+			fn test_card_2_1() {
+				test_card!($encoder, 2, $cmp, 1);
+			}
+
+			#[test]
+			fn test_card_2_2() {
+				test_card!($encoder, 2, $cmp, 2);
+			}
+
+			#[test]
+			fn test_card_3_1() {
+				test_card!($encoder, 3, $cmp, 1);
+			}
+
+			#[test]
+			fn test_card_3_2() {
+				test_card!($encoder, 3, $cmp, 2);
+			}
+
+			#[test]
+			fn test_card_3_3() {
+				test_card!($encoder, 3, $cmp, 3);
+			}
+
+			#[test]
+			fn test_card_4_2() {
+				test_card!($encoder, 4, $cmp, 2);
+			}
+
+			#[test]
+			fn test_card_4_3() {
+				test_card!($encoder, 4, $cmp, 3);
+			}
+
+			#[test]
+			fn test_card_4_4() {
+				test_card!($encoder, 4, $cmp, 4);
+			}
+
+			#[test]
+			fn test_card_5_3() {
+				test_card!($encoder, 5, $cmp, 3);
+			}
+
+			// #[test]
+			// fn test_card_12_7() {
+			// 	test_card!($encoder, 12, $cmp, 7);
+			// }
+		};
+	}
+
+	mod eq_recursive {
+		sorted_card_test_suite!(
+			{
+				let mut e = SortingNetworkEncoder::default();
+				let mut f = SortedEncoder::default();
+				f.set_strategy(SortedStrategy::Recursive);
+				e.set_sorted_encoder(f);
+				e
+			},
+			LimitComp::Equal
+		);
+	}
+
+	mod le_direct {
+		sorted_card_test_suite!(
+			{
+				let mut e = SortingNetworkEncoder::default();
+				let mut f = SortedEncoder::default();
+				f.set_strategy(SortedStrategy::Direct);
+				e.set_sorted_encoder(f);
+				e
+			},
+			LimitComp::LessEq
+		);
+	}
+
+	// Note; do not use expect_solutions, since Mixed yields incomplete solutions
+	mod le_mixed {
+		sorted_card_test_suite!(
+			{
+				let mut e = SortingNetworkEncoder::default();
+				let mut f = SortedEncoder::default();
+				f.set_strategy(SortedStrategy::Mixed(0.5));
+				e.set_sorted_encoder(f);
+				e
+			},
+			LimitComp::LessEq
+		);
+	}
 }
