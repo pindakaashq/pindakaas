71dc8e9efc8141d8f67a66f47431ce488833fbd0
Hendrik Bierlee <bierlee.henk@gmail.com>
2023-02-13 14:23:55 +1100

    Add model (although has conflicts storing vars)

--- crates/pindakaas/src/linear/totalizer.rs
+++ crates/pindakaas/src/linear/totalizer.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+use crate::Literal;
+use std::collections::{HashMap, HashSet};
+use std::rc::Rc;
 
 use itertools::Itertools;
 
@@ -39,77 +41,19 @@ impl<DB: ClauseDatabase, C: Coefficient> Encoder<DB, Linear<DB::Lit, C>> for Tot
 			.map(|(i, part)| {
 				IntVarOrd::from_part_using_le_ord(db, part, lin.k.clone(), format!("x_{i}")).into()
 			})
-			.collect::<Vec<_>>();
+			.collect::<Vec<IntVarEnc<DB::Lit, C>>>();
+		dbg!(&xs);
 
 		// The totalizer encoding constructs a binary tree starting from a layer of leaves
-		build_totalizer(
-			xs,
-			db,
-			&lin.cmp,
-			C::zero(),
-			0,
-			self.add_consistency,
-			None,
-			IntVarEnc::Const(*lin.k),
-		);
+		let mut model = build_totalizer(xs, &lin.cmp, *lin.k);
+		model.encode(db)?;
 		Ok(())
-	}
-}
 
-pub(crate) fn build_totalizer<DB: ClauseDatabase, C: Coefficient>(
-	layer: Vec<IntVarEnc<DB::Lit, C>>,
-	db: &mut DB,
-	cmp: &LimitComp,
-	l: C,
-	level: u32,
-	add_consistency: bool,
-	cutoff: Option<C>,
-	root: IntVarEnc<DB::Lit, C>,
-) -> IntVarEnc<DB::Lit, C> {
-	if layer.len() == 1 {
-		root
-	} else {
-		let next_layer = layer
-			.chunks(2)
-			.map(|children| {
-				let doms = match &children
-					.iter()
-					.map(|x| {
-						x.dom()
-							.clone()
-							.into_iter(..)
-							.map(|x| x.end - C::one())
-							.collect::<HashSet<_>>()
-					})
-					.collect::<Vec<_>>()[..]
-				{
-					[x] => x.clone(),
-					[left, right] => {
-						if layer.len() > 2 {
-							left.iter()
-								.cartesian_product(right.iter())
-								.map(|(&a, &b)| a + b)
-								.collect::<HashSet<_>>()
-						} else {
-							root.dom()
-								.into_iter(..)
-								.map(|x| x.end - C::one())
-								.collect::<HashSet<_>>()
-						}
-					}
-					_ => panic!(),
-				};
-				(children, doms)
-			})
-			.collect::<Vec<_>>();
-
-		let (children, doms): (Vec<_>, Vec<_>) = next_layer.into_iter().unzip();
-		let doms = propagate_layer_bounds(doms, cmp, root.ub());
-		let next_layer = children
-			.into_iter()
-			.zip(doms.into_iter())
-			.enumerate()
-			.map(|(node, (children, dom))| match children {
+		/*
+		model
+			.cons
+			.iter()
+			.map(|Lin { xs, cmp, k }| match children {
 				[x] => x.clone(),
 				[left, right] => {
 					// TODO re-establish binary heurstic
@@ -134,19 +78,230 @@ pub(crate) fn build_totalizer<DB: ClauseDatabase, C: Coefficient>(
 				_ => unreachable!(),
 			})
 			.collect::<Vec<_>>();
+			*/
 
-		build_totalizer(
-			next_layer,
-			db,
-			cmp,
-			l,
-			level + 1,
-			add_consistency,
-			cutoff,
-			root,
-		)
+		//Ok(())
 	}
 }
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct IntVar<C: Coefficient> {
+	dom: Vec<C>,
+}
+
+impl<C: Coefficient> IntVar<C> {
+	fn encode<DB: ClauseDatabase>(&self, db: &mut DB) -> IntVarEnc<DB::Lit, C> {
+		if self.dom.len() == 1 {
+			IntVarEnc::Const(self.dom.iter().next().unwrap().clone())
+		} else {
+			IntVarEnc::Ord(IntVarOrd::from_dom(
+				db,
+				self.dom
+					.iter()
+					.sorted()
+					.cloned()
+					.collect::<Vec<_>>()
+					.as_slice(),
+				format!("gt"),
+			))
+		}
+	}
+}
+
+#[derive(Debug)]
+struct Lin<C: Coefficient> {
+	xs: Vec<Rc<IntVar<C>>>,
+	cmp: LimitComp,
+	k: C,
+}
+
+#[derive(Debug)]
+struct Model<Lit: Literal, C: Coefficient> {
+	vars: HashMap<Rc<IntVar<C>>, IntVarEnc<Lit, C>>,
+	cons: Vec<Lin<C>>,
+}
+
+impl<Lit: Literal, C: Coefficient> Model<Lit, C> {
+	/*
+	fn var_enc<DB: ClauseDatabase<Lit = Lit>>(
+		&mut self,
+		db: &mut DB,
+		x: Rc<IntVar<C>>,
+	) -> &IntVarEnc<DB::Lit, C> {
+		self.vars.entry(x).or_insert_with(|| {})
+	}
+	*/
+
+	/*
+	fn encode_vars<DB: ClauseDatabase<Lit = Lit>>(
+		&self,
+		db: &mut DB,
+		enc: Vec<IntVarEnc<Lit, C>>,
+	) -> HashMap<&IntVar<C>, IntVarEnc<Lit, C>> {
+		dbg!(&enc);
+		self.vars
+			.iter()
+			.zip_longest(enc)
+			.map(|x_enc| match x_enc {
+				EitherOrBoth::Both(x, enc) => (x, enc),
+				EitherOrBoth::Left(x) => {
+					let enc = if x.dom.len() == 1 {
+						IntVarEnc::Const(x.dom.iter().next().unwrap().clone())
+					} else {
+						IntVarEnc::Ord(IntVarOrd::from_dom(
+							db,
+							x.dom
+								.iter()
+								.sorted()
+								.cloned()
+								.collect::<Vec<_>>()
+								.as_slice(),
+							format!("gt"),
+						))
+					};
+					(x, enc)
+				}
+				EitherOrBoth::Right(_) => unreachable!(),
+			})
+			.collect::<HashMap<_, _>>()
+	}
+	*/
+
+	fn encode<DB: ClauseDatabase<Lit = Lit>>(&mut self, db: &mut DB) -> Result {
+		//let encs = self.encode_vars(db, encs);
+
+		for con in &self.cons {
+			let Lin { xs, cmp, k: _ } = con;
+			assert!(con.xs.len() == 3);
+
+			for x in xs {
+				if !self.vars.contains_key(x) {
+					self.vars.insert(x.clone(), x.encode(db));
+				}
+			}
+			//     self.var_enc(db, x.clone()).clone())
+
+			let (x, y, z) = (&self.vars[&xs[0]], &self.vars[&xs[1]], &self.vars[&xs[2]]);
+
+			TernLeEncoder::default()
+				.encode(
+					db,
+					//&TernLeConstraint::new(&v[xs[0].as_ref()], v[xs[1]], cmp.clone(), v[xs[2]]),
+					&TernLeConstraint::new(&x, &y, cmp.clone(), &z),
+				)
+				.unwrap();
+		}
+
+		dbg!(&self);
+
+		Ok(())
+	}
+}
+
+fn build_totalizer<Lit: Literal, C: Coefficient>(
+	xs: Vec<IntVarEnc<Lit, C>>,
+	cmp: &LimitComp,
+	k: C,
+) -> Model<Lit, C> {
+	let mut model = Model {
+		vars: xs
+			.into_iter()
+			.map(|x| {
+				let var = IntVar {
+					dom: x.dom().iter(..).map(|d| d.end - C::one()).collect(),
+				};
+				(Rc::new(var), x)
+			})
+			.collect(),
+		cons: vec![],
+	};
+
+	let mut layer = model
+		.vars
+		.iter()
+		.map(|(l, _)| l.clone())
+		.collect::<Vec<_>>();
+
+	dbg!(&layer);
+	while layer.len() > 1 {
+		let mut next_layer = Vec::<Rc<IntVar<C>>>::new();
+		for children in layer.chunks(2) {
+			match &children[..] {
+				[x] => {
+					next_layer.push(x.clone());
+				}
+				[left, right] => {
+					let parent = Rc::new(IntVar {
+						dom: if layer.len() == 2 {
+							vec![k]
+						} else {
+							left.dom
+								.iter()
+								.cartesian_product(right.dom.iter())
+								.map(|(&a, &b)| a + b)
+								.filter(|x| x <= &k)
+								.sorted()
+								.dedup()
+								.collect()
+						},
+					});
+					model.cons.push(Lin {
+						xs: vec![left.clone(), right.clone(), parent.clone()],
+						cmp: cmp.clone(),
+						k: C::zero(),
+					});
+					next_layer.push(parent);
+				}
+				_ => panic!(),
+			}
+		}
+		layer = next_layer;
+	}
+
+	model
+
+	/*
+			let (children, doms): (Vec<_>, Vec<_>) = next_layer.into_iter().unzip();
+			let doms = propagate_layer_bounds(doms, cmp, root.ub());
+			dbg!(&doms);
+			let next_layer = children
+				.into_iter()
+				.zip(doms.into_iter())
+				.enumerate()
+				.map(|(node, (children, dom))| match children {
+					[x] => x.clone(),
+					[left, right] => {
+						// TODO re-establish binary heurstic
+						let parent = if dom.len() == 1 {
+							IntVarEnc::Const(dom.into_iter().next().unwrap())
+						} else {
+							IntVarEnc::Ord(IntVarOrd::from_dom(
+								db,
+								dom.into_iter().sorted().collect::<Vec<_>>().as_slice(),
+								format!("gt_{}_{}", level + 1, node + 1),
+							))
+						};
+
+						TernLeEncoder::default()
+							.encode(
+								db,
+								&TernLeConstraint::new(&left, &right, cmp.clone(), &parent),
+							)
+							.unwrap();
+						parent
+					}
+					_ => unreachable!(),
+				})
+				.collect::<Vec<_>>();
+
+			build_totalizer(next_layer, db, cmp, level + 1)
+		}
+	*/
+}
+
+//add_consistency,
+//cutoff,
+//root,
 
 fn propagate_layer_bounds<C: Coefficient>(
 	doms: Vec<HashSet<C>>,
@@ -181,7 +336,7 @@ fn propagate_layer_bounds<C: Coefficient>(
 	let doms = doms
 		.into_iter()
 		.map(|dom| {
-			let dom_lb = dom.iter().min().unwrap().clone();
+			let dom_lb = *dom.iter().min().unwrap();
 			dom.into_iter()
 				.filter(|d| *d + layer_lb - dom_lb <= k)
 				.collect()
