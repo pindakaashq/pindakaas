c022f409de88a8638bc9304c62615bfd9e5616a7
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-05-21 13:50:08 +0000

    Fix test

--- crates/pyndakaas/python/tests/test_encoding.py
+++ crates/pyndakaas/python/tests/test_encoding.py
@@ -56,5 +56,5 @@ def test_conditions():
     f = pindakaas.CNF()
     x, y, p = f.new_vars(3)
     f.add_encoding(x ^ y, conditions=[p])
-    assert f.to_dimacs() == ""
+    assert f.to_dimacs() == "p cnf 3 2\n3 1 2 0\n3 -1 -2 0\n"
 
