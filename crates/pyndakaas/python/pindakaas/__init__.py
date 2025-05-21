096e1e912db6a59aa0e65572137c76881c22147a
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-05-21 09:12:04 +0000

    Export exceptions so users can catch them

--- crates/pyndakaas/python/pindakaas/__init__.py
+++ crates/pyndakaas/python/pindakaas/__init__.py
@@ -1,7 +1,7 @@
 import pindakaas.solver
 
 from .encoding import CNF, WCNF, ClauseDatabase, Constraint
-from .pindakaas import Formula, Lit, Encoder
+from .pindakaas import Formula, Lit, Encoder, Unsatisfiable, InvalidEncoder
 
 __doc__ = pindakaas.__doc__
 __all__ = ["Constraint", "ClauseDatabase", "CNF", "Encoder", "Formula", "Lit", "WCNF"]
