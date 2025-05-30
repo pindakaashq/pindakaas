eb60d5fe83305e4d74e00f551563decc1d6d696f
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-05-30 16:24:47 +0000

    Add exceptions to `__all__`

--- crates/pyndakaas/python/pindakaas/__init__.py
+++ crates/pyndakaas/python/pindakaas/__init__.py
@@ -4,4 +4,14 @@ from .encoding import CNF, WCNF, ClauseDatabase, Constraint
 from .pindakaas import Formula, Lit, Encoder, Unsatisfiable, InvalidEncoder
 
 __doc__ = pindakaas.__doc__
-__all__ = ["Constraint", "ClauseDatabase", "CNF", "Encoder", "Formula", "Lit", "WCNF"]
+__all__ = [
+    "Constraint",
+    "ClauseDatabase",
+    "CNF",
+    "Encoder",
+    "Formula",
+    "Lit",
+    "WCNF",
+    "Unsatisfiable",
+    "InvalidEncoder",
+]
