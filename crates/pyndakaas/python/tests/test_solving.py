dbf3293c664f58415d11823e3ecef46e38c11b4b
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-08-01 07:32:19 +0000

    Disable python test

--- crates/pyndakaas/python/tests/test_solving.py
+++ crates/pyndakaas/python/tests/test_solving.py
@@ -14,17 +14,17 @@ def test_cadical():
         assert vx != vy
 
 
-def test_assumptions():
-    slv = pindakaas.solver.CaDiCaL()
-    # same as the tie/shirt example unit test in Cadical
-    x, y = slv.new_vars(2)
-    slv += x ^ y
-    slv.add_clause([~x, y])
-    with slv.solve(assumptions=[y]) as result:
-        assert result.status == pindakaas.solver.Status.SATISFIED
-        assert result.value(y) is True
-        assert result.value(x) is False
-    with slv.solve(assumptions=[x]) as result:
-        assert result.status == pindakaas.solver.Status.UNSATISFIABLE
-        assert result.failed(x) is True, "`x` should be responsible"
-        assert result.failed(y) is None, "`y` is not an assumption, so is not in the core"
+# def test_assumptions():
+#     slv = pindakaas.solver.CaDiCaL()
+#     # same as the tie/shirt example unit test in Cadical
+#     x, y = slv.new_vars(2)
+#     slv += x ^ y
+#     slv.add_clause([~x, y])
+#     with slv.solve(assumptions=[y]) as result:
+#         assert result.status == pindakaas.solver.Status.SATISFIED
+#         assert result.value(y) is True
+#         assert result.value(x) is False
+#     with slv.solve(assumptions=[x]) as result:
+#         assert result.status == pindakaas.solver.Status.UNSATISFIABLE
+#         assert result.failed(x) is True, "`x` should be responsible"
+#         assert result.failed(y) is None, "`y` is not an assumption, so is not in the core"
