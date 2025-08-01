c2747d2b8efb1303afce8679c5fafb44f2c9ad34
Hendrik 'Henk' Bierlee <bierlee.henk@gmail.com>
2025-08-01 08:52:13 +0000

    Try single solve call

--- crates/pyndakaas/python/tests/test_solving.py
+++ crates/pyndakaas/python/tests/test_solving.py
@@ -20,11 +20,13 @@ def test_assumptions():
     x, y = slv.new_vars(2)
     slv += x ^ y
     slv.add_clause([~x, y])
-    with slv.solve(assumptions=[y]) as result:
-        assert result.status == pindakaas.solver.Status.SATISFIED
-        assert result.value(y) is True
-        assert result.value(x) is False
+    # with slv.solve(assumptions=[y]) as result:
+    #     print("res", result._mapping)
+    #     assert result.status == pindakaas.solver.Status.SATISFIED
+    #     assert result.value(y) is True
+    #     assert result.value(x) is False
     with slv.solve(assumptions=[x]) as result:
+        print("res", result._mapping)
         assert result.status == pindakaas.solver.Status.UNSATISFIABLE
         assert result.failed(x) is True, "`x` should be responsible"
         assert result.failed(y) is None, "`y` is not an assumption, so is not in the core"
